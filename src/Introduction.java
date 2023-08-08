691d6425de3def0ea05cdf3ee21c845a338031d0
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-08 12:03:57 +0530

    graph

--- src/Introduction.java
+++ src/Introduction.java
@@ -1,5 +1,16 @@
     public class Introduction {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println();
+        System.out.println(
+                "A Graph is a non-linear data structure consisting of vertices and edges. The vertices are sometimes also \n" +
+                " referred to as nodes and the edges are lines or arcs that connect any two nodes in the graph. More formally \n" +
+                " Graph is composed of a set of vertices( V ) and a set of edges( E ). The graph is denoted by G(E, V).\n");
+
+        System.out.println("Components of a Graph\n" +
+                "Vertices: Vertices are the fundamental units of the graph. Sometimes, vertices are also known as vertex\n" +
+                " or nodes. Every node/vertex can be labeled or unlabelled.\n" +
+                "Edges: Edges are drawn or used to connect two nodes of the graph. It can be ordered pair of nodes in a \n" +
+                " directed graph. Edges can connect any two nodes in any possible way. There are no rules. Sometimes, \n" +
+                " edges are also known as arcs. Every edge can be labeled/unlabelled.");
     }
 }
