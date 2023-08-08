5ad4ecca98c8f7af39392b6af322f7751650a282
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-08 13:11:15 +0530

    graph

--- src/Implementation.java
+++ src/Implementation.java
@@ -23,10 +23,20 @@ public class Implementation {
             graph[i] = new ArrayList<>();
         }
 
+        // 0 --> Vertex
         graph[0].add(new Edge(0, 1, 5));
+        // 1 ->> vertex
         graph[1].add(new Edge(1, 2, 1));
-        graph[0].add(new Edge(0, 1, 5));
-        graph[0].add(new Edge(0, 1, 5));
-        graph[0].add(new Edge(0, 1, 5));
+        graph[1].add(new Edge(1, 3, 3));
+        graph[1].add(new Edge(1, 0, 5));
+        // 2 --> vertex
+        graph[2].add(new Edge(2, 1, 1));
+        graph[2].add(new Edge(2, 3, 1));
+        graph[2].add(new Edge(2, 4, 2));
+        // 3 --> vertex
+        graph[3].add(new Edge(3,1,3));
+        graph[3].add(new Edge(3,2,1));
+        // 4 --> vertex
+        graph[4].add(new Edge(4,2,2));
     }
 }
