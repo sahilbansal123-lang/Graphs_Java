54926d6126c4b2574a2d3d2d83ea59815324c6b3
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-10 13:04:22 +0530

    graph

--- src/HasPath.java
+++ src/HasPath.java
@@ -45,12 +45,24 @@ public class HasPath {
 
     }
 
-    public static boolean hasPath(ArrayList<Edge> graph[]) {
+    public static boolean hasPath(ArrayList<Edge> graph[], int src, int dest, boolean vis[]) {
 
-
+        if (src == dest) {
+            return true;
+        }
+        vis[src] = true;
+        for (int i = 0; i < graph[src].size(); i++) {
+            Edge e = graph[src].get(i);
+            if (!vis[e.dest] && hasPath(graph, e.dest, dest, vis)) {
+                return true;
+            }
+        }
         return false;
     }
     public static void main(String[] args) {
-
+        int v = 7;
+        ArrayList<Edge> [] graph = new ArrayList[v];
+        createGraph(graph);
+        System.out.println(hasPath(graph, 0, 4, new boolean[v]));
     }
 }
