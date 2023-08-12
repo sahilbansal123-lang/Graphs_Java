e151264478a78c471adaaca26f9ce4c1d82f717e
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-12 11:24:35 +0530

    graph

---
+++ src/ConnectedComponentsDfs.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class ConnectedComponentsDfs {
+    public static class Edge{
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int s, int d, int wt) {
+            this.src = s;
+            this.dest = d;
+            this.weight = wt;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
