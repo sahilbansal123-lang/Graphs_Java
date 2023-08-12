f5ffbbd51eafa956e5aa64cbb89ea164fd0e74aa
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-12 11:29:00 +0530

    graph

--- src/ConnectedComponentsDfs.java
+++ src/ConnectedComponentsDfs.java
@@ -13,6 +13,18 @@ public class ConnectedComponentsDfs {
         }
     }
 
+    public static void dfsUtil(ArrayList<Edge>[] graph, int curr, boolean isVis[]) {
+        System.out.print(curr + " ");
+        isVis[curr] = true;
+
+        for (int i = 0; i < graph[curr].size(); i++) {
+            Edge e = graph[curr].get(i);
+            if (!isVis[e.dest]) {
+                dfsUtil(graph, e.dest, isVis);
+            }
+        }
+    }
+
     public static void main(String[] args) {
 
     }
