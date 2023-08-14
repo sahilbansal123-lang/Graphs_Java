7a0e2836a0248d23057c77466d312fb2e3a47fdb
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-14 11:41:39 +0530

    graph

--- src/cycleDetectionUndirected.java
+++ src/cycleDetectionUndirected.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class cycleDetectionUndirected {
 
     public static class Edge{
@@ -10,7 +12,28 @@ public class cycleDetectionUndirected {
         }
     }
 
+    static void createGraph(ArrayList<Edge>[] graph) {
 
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 0));
+
+        graph[4].add(new Edge(4, 3));
+
+    }
 
     public static void main(String[] args) {
 
