5768e9e145119f3fd4db23a7338bd20b81092777
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-15 11:38:09 +0530

    graph

--- src/BipartiteGraph.java
+++ src/BipartiteGraph.java
@@ -1,5 +1,36 @@
+import java.util.ArrayList;
+
 public class BipartiteGraph {
 
+    public static class Edge {
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+
+        graph[1].add(new Edge(1, 3));
+        graph[1].add(new Edge(1, 0));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+
+
+    }
 
     public static void main(String[] args) {
 
