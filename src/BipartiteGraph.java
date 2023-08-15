5ff7dce622336a5aa8eea301165e64507c7a1ee7
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-15 11:56:03 +0530

    graph

--- src/BipartiteGraph.java
+++ src/BipartiteGraph.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class BipartiteGraph {
 
@@ -33,6 +35,41 @@ public class BipartiteGraph {
         graph[4].add(new Edge(4, 2));
         graph[4].add(new Edge(4, 3));
 
+    }
+
+    public static boolean isBipartite(ArrayList<Edge>[] graph) {
+        int color[] = new int[graph.length];
+        // -1 == no color
+        //  0 == yellow color
+        //  1 == blue color
+
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
+        }
+
+        Queue<Integer> q = new LinkedList<>();
+
+        for (int i = 0; i < graph.length; i++) {
+            if (color[i] == -1) {
+                q.add(i);
+                color[i] = 0;
+
+                while (!q.isEmpty()) {
+                    int curr = q.remove();
+                    for (int j = 0; j < graph[curr].size(); j++) {
+                        Edge e = graph[curr].get(j);
+                        if (color[e.dest] == -1) {
+                            int nextColor = color[curr] == 0 ? 1 : 0;
+                            color[e.dest] = nextColor;
+                            q.add(e.dest);
+                        } else if (color[e.dest] == color[curr]) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 
     public static void main(String[] args) {
