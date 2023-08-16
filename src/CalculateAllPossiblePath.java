37e778975ffda1ef01aee8df4b278a79c1721936
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-16 12:39:32 +0530

    graph

--- src/CalculateAllPossiblePath.java
+++ src/CalculateAllPossiblePath.java
@@ -1,2 +1,33 @@
+import java.util.ArrayList;
+
 public class CalculateAllPossiblePath {
+
+    public static class Edge {
+        int src;
+        int dest;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 3));
+        graph[3].add(new Edge(3, 1));
+        graph[2].add(new Edge(2, 3));
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+
+    }
+
+
+    public static void main(String[] args) {
+
+    }
 }
