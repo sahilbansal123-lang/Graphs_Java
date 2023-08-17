4dbbe12c086bb2dbbdd4cd5fc673e831f3cfb5bc
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-17 12:29:43 +0530

    graph

--- src/CycleDetection/CycleDetectionUndirectedBfs.java
+++ src/CycleDetection/CycleDetectionUndirectedBfs.java
@@ -1,7 +1,41 @@
 package CycleDetection;
 
+import java.util.ArrayList;
+
 public class CycleDetectionUndirectedBfs {
 
+    public static class Edge{
+        int src;
+        int dest;
+
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    static void createGraph(ArrayList<Edge>[] graph) {
+
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[0].add(new Edge(0, 3));
+
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 2));
+
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 1));
+
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 0));
+
+        graph[4].add(new Edge(4, 3));
+
+    }
 
     public static void main(String[] args) {
 
