22c262aa332b9b959e57afe8f61814002e4666ad
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-17 10:23:44 +0530

    graph

--- src/DijktrawsAlgorithm.java
+++ src/DijktrawsAlgorithm.java
@@ -1,2 +1,28 @@
+import java.util.ArrayList;
+
 public class DijktrawsAlgorithm {
+
+    public static class Edge {
+        int src;
+        int dest;
+        public Edge(int s, int d) {
+            this.src = s;
+            this.dest = d;
+        }
+    }
+
+    public static void createGraph(ArrayList<Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
+
+        graph[3].add(new Edge(3, 1));
+        graph[2].add(new Edge(2, 3));
+        graph[4].add(new Edge(4, 0));
+        graph[4].add(new Edge(4, 1));
+        graph[5].add(new Edge(5, 0));
+        graph[5].add(new Edge(5, 2));
+
+    }
+
 }
