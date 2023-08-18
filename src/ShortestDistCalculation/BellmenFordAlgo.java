840d7df0f485944b1be8319c878b3dcde14a5a93
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-18 11:12:54 +0530

    graph

--- src/ShortestDistCalculation/BellmenFordAlgo.java
+++ src/ShortestDistCalculation/BellmenFordAlgo.java
@@ -1,7 +1,5 @@
 package ShortestDistCalculation;
 
-import CycleDetection.CycleDetectionUndirectedBfs;
-
 import java.util.ArrayList;
 
 public class BellmenFordAlgo {
@@ -18,34 +16,46 @@ public class BellmenFordAlgo {
         }
     }
 
-    static void createGraph(ArrayList<Edge>[] graph) {
+    static void createGraph(ArrayList<Edge> graph) {
 
-        for (int i = 0; i < graph.length; i++) {
-            graph[i] = new ArrayList<>();
-        }
-
-        graph[0].add(new Edge(0, 1, 2));
-        graph[0].add(new Edge(0, 2, 4));
-
-        graph[1].add(new Edge(1, 2, -4));
-
-        graph[2].add(new Edge(2, 3, 2));
-
-        graph[3].add(new Edge(3, 4, 4));
-
-        graph[4].add(new Edge(4, 1, -1));
-
+        graph.add(new Edge(0, 1, 2));
+        graph.add(new Edge(0, 2, 4));
+        graph.add(new Edge(1, 2, -4));
+        graph.add(new Edge(2, 3, 2));
+        graph.add(new Edge(3, 4, 4));
+        graph.add(new Edge(4, 1, -1));
     }
 
-    public static void shortestDist(ArrayList<Edge>[] graph) {
+    public static void shortestDist(ArrayList<Edge> graph, int src, int v) {
+        int[] dist = new int[v];
+        for (int i = 0; i < dist.length; i++) {
+            if (i != src) {
+                dist[i] = Integer.MAX_VALUE;
+            }
+        }
 
+        for (int i = 0; i < v-1; i++) {
+            for (int j = 0; j < graph.size(); j++) {
+                Edge e = graph.get(j);
+                int u = e.src;
+                int V = e.dest;
+                int wt = e.weight;
+                if (dist[u] != Integer.MAX_VALUE && dist[u] + wt < dist[V]) {
+                    dist[V] = dist[u] + wt;
+                }
+            }
+        }
+        for (int i = 0; i < dist.length; i++) {
+            System.out.print(dist[i] + " ");
+        }
+        System.out.println();
     }
 
     public static void main(String[] args) {
         int v = 5;
-        ArrayList<Edge>[] graph = new ArrayList[v];
-        for (int i = 0; i < graph.length; i++) {
-            createGraph(graph);
-        }
+        ArrayList<Edge> graph = new ArrayList<>();
+        createGraph(graph);
+
+        shortestDist(graph, 0, v);
     }
 }
