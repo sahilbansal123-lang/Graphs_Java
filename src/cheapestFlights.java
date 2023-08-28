ca175d5fd99f86fe5e39939355ae8f0e913df5f0
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-28 11:38:30 +0530

    graph

--- src/cheapestFlights.java
+++ src/cheapestFlights.java
@@ -1,5 +1,30 @@
 public class cheapestFlights {
 
+    public static class Edge{
+        int src;
+        int dest;
+        int weight;
+
+        public Edge(int s, int d, int wt) {
+            this.src = s;
+            this.dest = d;
+            this.weight = wt;
+        }
+    }
+
+    public static class Info {
+        int vertex;
+        int cost;
+        int stops;
+
+        public Info(int v, int c, int s) {
+            this.vertex = v;
+            this.cost = c;
+            this.stops = s;
+        }
+    }
+
+
 
 
 
