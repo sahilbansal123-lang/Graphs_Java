41b820b9678fa49481dfd1e10dee022a2718bf13
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-29 10:41:40 +0530

    graph

--- src/ConnectingCitiesWithMinCost.java
+++ src/ConnectingCitiesWithMinCost.java
@@ -1,2 +1,14 @@
 public class ConnectingCitiesWithMinCost {
+
+    public static class Edge {}
+
+
+    public static void main(String[] args) {
+        int cities[][] = {{0, 1, 2, 3, 4},
+                          {1, 0, 5, 0, 7},
+                          {2, 5, 0, 6, 0},
+                          {3, 0, 6, 0, 0},
+                          {4, 7, 0, 0, 0}};
+
+    }
 }
