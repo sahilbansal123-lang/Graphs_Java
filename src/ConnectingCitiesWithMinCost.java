6bfb573efef419c8f83a6e9a418121ce94849e39
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-29 11:01:37 +0530

    graph

--- src/ConnectingCitiesWithMinCost.java
+++ src/ConnectingCitiesWithMinCost.java
@@ -1,3 +1,6 @@
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+
 public class ConnectingCitiesWithMinCost {
 
     public static class Edge implements Comparable<Edge>{
@@ -15,7 +18,25 @@ public class ConnectingCitiesWithMinCost {
 
     public static void MinCost(int cities[][]) {
         boolean isVis[] = new boolean[cities.length];
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
 
+        int finalCost = 0;
+        pq.add(new Edge(0, 0));
+
+        while (!pq.isEmpty()) {
+            Edge curr = pq.remove();
+            if (!isVis[curr.dest]) {
+                isVis[curr.dest] = true;
+                finalCost += curr.cost;
+
+                for (int i = 0; i < cities[curr.dest].length; i++) {
+                    if (cities[curr.dest][i] != 0) {
+                        pq.add(new Edge(i, cities[curr.dest][i]));
+                    }
+                }
+            }
+        }
+        System.out.println(finalCost);
     }
 
     public static void main(String[] args) {
@@ -25,5 +46,6 @@ public class ConnectingCitiesWithMinCost {
                           {3, 0, 6, 0, 0},
                           {4, 7, 0, 0, 0}};
 
+        MinCost(cities);
     }
 }
