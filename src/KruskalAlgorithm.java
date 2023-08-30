fec7ccb4e7a8b5716f5bd1b0c15dbca86e50106f
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-08-30 11:06:06 +0530

    graph

--- src/KruskalAlgorithm.java
+++ src/KruskalAlgorithm.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class KruskalAlgorithm {
 
@@ -18,7 +19,27 @@ public class KruskalAlgorithm {
         }
     }
 
+    public static void createGraph(ArrayList<MSTPrimsAlgo.Edge>[] graph) {
+        for (int i = 0; i < graph.length; i++) {
+            graph[i] = new ArrayList<>();
+        }
 
+        graph[0].add(new MSTPrimsAlgo.Edge(0, 1, 10));
+        graph[0].add(new MSTPrimsAlgo.Edge(0, 2, 15));
+        graph[0].add(new MSTPrimsAlgo.Edge(0, 3, 30));
+
+        graph[1].add(new MSTPrimsAlgo.Edge(1, 3, 40));
+        graph[1].add(new MSTPrimsAlgo.Edge(1, 0, 10));
+
+        graph[2].add(new MSTPrimsAlgo.Edge(2, 3, 50));
+        graph[2].add(new MSTPrimsAlgo.Edge(2, 0, 15));
+
+        graph[3].add(new MSTPrimsAlgo.Edge(3, 0, 30));
+        graph[3].add(new MSTPrimsAlgo.Edge(3, 2, 50));
+        graph[3].add(new MSTPrimsAlgo.Edge(3, 1, 40));
+
+
+    }
 
     public static void main(String[] args) {
 
