18879a355affcb95c6c5948f5bdbc876dbd96937
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-02 12:26:04 +0530

    graph

--- src/FloodFillAlgo.java
+++ src/FloodFillAlgo.java
@@ -1,2 +1,16 @@
 public class FloodFillAlgo {
+
+    public static int[][] floodFill(int image[][], int sr, int sc, int color) {
+        boolean isVis[][] = new boolean[image.length][image[0].length];
+        helper(image, sr, sc, color, isVis, image[sr][sc]);
+        return image;
+    }
+
+    public static void helper(int[][] image, int sr, int sc, int color, boolean[][] isVis, int origColor) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
 }
