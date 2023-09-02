27c0dcdb543c3d54d6138dac2d9a943f3baf99ee
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-09-02 12:30:31 +0530

    graph

--- src/FloodFillAlgo.java
+++ src/FloodFillAlgo.java
@@ -7,7 +7,19 @@ public class FloodFillAlgo {
     }
 
     public static void helper(int[][] image, int sr, int sc, int color, boolean[][] isVis, int origColor) {
+        if (sr < 0 || sc < 0 || sr >= image.length || sc >= image[0].length || isVis[sr][sc] || image[sr][sc] != origColor) {
+            return;
+        }
 
+        image[sr][sc] = color;
+        //up
+        helper(image, sr - 1, sc, color, isVis, origColor);
+        // down
+        helper(image, sr + 1, sc, color, isVis, origColor);
+        // left
+        helper(image, sr, sc - 1, color, isVis, origColor);
+        // right
+        helper(image, sr, sc + 1, color, isVis, origColor);
     }
 
     public static void main(String[] args) {
